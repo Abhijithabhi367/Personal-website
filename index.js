1f53ea81ba998de6e413b179b1a1ec0c1cf3de9f
abhijithabhiakl <abhiabhijith367@gmail.com>
2021-05-10 20:40:42 +0530

    Dice Game updated on website

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-var a = 1000;
-var b = 6;
-console.log("a is " + a + " b is " + b);
-var c = a;
-a = b;
-b = c;
-console.log("now, a is "+a+" b is" + b);
+var randomNo01 = Math.floor((Math.random()*6)+1);
+var randomNo02 = Math.floor((Math.random()*6)+1);
+var dice01 = "dice" + randomNo01 + ".png";
+var dice02 = "dice" + randomNo02 + ".png";
+var randomDice01  = "images\\" + dice01 ;
+var randomDice02  = "images\\" + dice02 ;
+ document.querySelector(".img1").setAttribute("src",randomDice01 );
+ document.querySelector(".img2").setAttribute("src",randomDice02 );
+    if (randomNo01 > randomNo02 ) {
+        document.querySelector("h1").textContent = "Player 1 Wins";
+    } else if (randomNo01 < randomNo02){
+        document.querySelector("h1").textContent = "Player 2 Wins";
+    } else if (randomNo01 = randomNo02) {
+        document.querySelector("h1").textContent = "Draw !";
+    }
